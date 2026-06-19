72b29888c182f98bac7bc05c041f4d107647576d
Kewer Admin <kewer@localhost>
2026-06-19 23:04:54 +0700

    db: export current local database for production import

--- sql/skd_cat_bkn_production.sql
+++ sql/skd_cat_bkn_production.sql
@@ -1986,4 +1986,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-06-19 23:04:21
+-- Dump completed on 2026-06-19 23:04:38
